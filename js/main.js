b08d1c610b9973f298393a0e26ae8649cfcafb25
ShantalGovantes1792 <shangovm@gmail.com>
2016-11-30 15:23:19 -0600

    cambios en boostrap

--- js/main.js
+++ js/main.js
@@ -6,4 +6,37 @@ $(document).ready(function(){
 	$("#wid").mouseout(function(){
 		$(this).css("background-color", "#27102D");
     });
+
+	// IMAGENES
+	$(".img-click-expand").mouseover(function(){
+		console.log("hola");
+		 $(this).slideUp(2000).slideDown(2000);
+	});
+
+	$("#img-anima").click(function(){
+        $("#img-2").animate({
+            left: '250px',
+            opacity: '0.5',
+            height: '150px',
+            width: '150px'
+         });
+
+        $("#img-2").animate({
+            opacity: '1',
+            height: '100%',
+            width: '100%'
+        });   
+              
+
+     });
+
+    $("#chica-1").click(function(){
+        $("#chica-1-animate").animate({
+            height: 'toggle'
+        });
+    });
+
+
 });
+
+
